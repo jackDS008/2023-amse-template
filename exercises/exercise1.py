a28eb882ee3599943e60722b7420455b79e18774
Jannik Kiesel <jannik.kiesel@fau.de>
2023-05-18 16:36:07 +0200

    Refactered code using only allowed libraries

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,50 +1,29 @@
-import requests
-import csv
-import sqlite3
+import pandas as pd
+from sqlalchemy import create_engine, Integer, Text, CHAR, Float
 
-# Define the URL of the data source
+# Read the CSV file into a pandas DataFrame
 url = "https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv"
+df = pd.read_csv(url, sep=";")
 
-# Define the SQLite database and table names
-database_name = "airports.sqlite"
-table_name = "airports"
+# Define the SQLite database connection
+engine = create_engine("sqlite:///airports.sqlite")
 
-# Fetch the data from the URL
-response = requests.get(url)
-data = response.text
+# Convert columns to appropriate SQLite types
+column_types = {
+    "column_1": Integer(),
+    "column_2": Text(),
+    "column_3": Text(),
+    "column_4": Text(),
+    "column_5": Text(),
+    "column_6": Text(),
+    "column_7": Float(),
+    "column_8": Float(),
+    "column_9": Integer(),
+    "column_10": Float(),
+    "column_11": CHAR(),
+    "column_12": Text(),
+    "geo_punkt": Text()
+}
 
-# Parse the CSV data
-csv_reader = csv.reader(data.splitlines(), delimiter=';')
-rows = list(csv_reader)
-
-# Extract column names from the first row
-column_names = [column.strip() for column in rows[0]]
-
-# Remove the first row (column names)
-rows = rows[1:]
-
-# Create the SQLite database connection
-conn = sqlite3.connect(database_name)
-cursor = conn.cursor()
-
-# Generate the CREATE TABLE statement
-create_table_sql = f"CREATE TABLE IF NOT EXISTS {table_name} ("
-for column_name in column_names:
-    if column_name in ['column_1', 'column_9']:
-        create_table_sql += f"{column_name} INTEGER, "
-    elif column_name in ['column_7', 'column_8']:
-        create_table_sql += f"{column_name} REAL, "
-    else:
-        create_table_sql += f"{column_name} TEXT, "
-create_table_sql = create_table_sql[:-2] + ")"  # Remove the last comma and space
-
-# Create the table in the database
-cursor.execute(create_table_sql)
-
-# Generate the INSERT INTO statement and insert data row by row
-insert_sql = f"INSERT INTO {table_name} VALUES ({', '.join(['?'] * len(column_names))})"
-cursor.executemany(insert_sql, rows)
-
-# Commit the changes and close the database connection
-conn.commit()
-conn.close()
+# Write the DataFrame to the SQLite database table
+df.to_sql("airports", engine, if_exists="replace", index=False, dtype=column_types)
