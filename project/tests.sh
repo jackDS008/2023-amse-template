6b2333925098996543b94767fab53de5a52a03d8
Jannik Kiesel <jannik.kiesel@fau.de>
2023-06-27 13:54:50 +0200

    test paths

--- project/tests.sh
+++ project/tests.sh
@@ -1,9 +1,38 @@
 #!/bin/bash
 
-# Test
-echo "Testing..."
-python tests.py
+filePath_parkverstoesse_bonn="./data/parkverstoesse_bonn.db"
+file_weather_data="./data/weather_data.db"
+filePathPipeline_parkverstoesse_bonn="./data/pipeline_car_park_occupancy.py"
+filePathPipeline_weather_data="./data/datapipeline_weather.py"
+
+main() {
+    # Run Pipelines
+    command="python $(realpath $filePathPipeline_parkverstoesse_bonn)"
+    eval $command
+    command="python $(realpath $filePathPipeline_parkverstoesse_bonn)"
+    eval $command
+
+    # Test database existence
+    echo "Test database existence"
+    # Check if the file exists
+    if [[ -e $filePath_parkverstoesse_bonn && -e $file_weather_data ]]; then
+        echo "File exists"
+        echo "Test completed"
+    else
+        echo "Test failed"
+        if [[ ! -e $filePath_parkverstoesse_bonn ]]; then
+            echo "File $filePath_parkverstoesse_bonn does not exist"
+        fi
+        if [[ ! -e $file_weather_data ]]; then
+            echo "File $file_weather_data does not exist"
+        fi
+    fi
+}
+
+# Call the main function
+main
 
 # All tests passed
-#echo "All tests passed"
-#exit 0
+echo "All tests passed"
+exit 0
+
